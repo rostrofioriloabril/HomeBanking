0f016c4d9062665b4543815a330fdb8f9da462b8
Abril Micaela Rostro Fiorilo <rostrofioriloabril@gmail.com>
2022-05-31 00:45:08 -0300

    Agregandole eventos a los botones del home, la pagina ya funciona y es responsive

---
+++ main2.js
@@ -0,0 +1,62 @@
+///--------------------CONTAINER 1-------------------------//
+var containerPrincipal = document.getElementById('container_1');
+var meOlvide = document.getElementById('olvidar');
+var nuevoCliente = document.getElementById('nuevo1');
+
+
+//--------------------CONTAINER 2-------------------------//
+var containerAsociate = document.getElementById('container_2');
+
+var registro = document.getElementById('registro')
+var iniciarSesion = document.getElementById('inicia');
+
+
+//--------------------CONTAINER 3-------------------------//
+var containerRecupera = document.getElementById('container_3');
+var btnIngresar = document.getElementById('ingresar');
+var nuevoCliente2 = document.getElementById('nuevo2');
+
+
+
+
+
+
+
+
+
+//-------------FUNCIONES Y BOTONES------//
+
+function nuevoRegistro(event){
+    event.preventDefault();
+    containerAsociate.style.display='block'
+    containerPrincipal.style.display='none';
+    containerRecupera.style.display='none';
+}
+nuevoCliente.addEventListener('click', nuevoRegistro);
+
+function datosOlvidados(event){
+    event.preventDefault(),
+    containerPrincipal.style.display='none';
+    containerAsociate.style.display='none';
+    containerRecupera.style.display='block';
+}
+meOlvide.addEventListener('click', datosOlvidados);
+
+
+iniciarSesion.addEventListener('click', iniciarSesionFunction);
+registro.addEventListener('click',nuevoRegistro);
+
+
+
+function iniciarSesionFunction (event){
+    event.preventDefault();
+    containerPrincipal.style.display='block';
+    containerAsociate.style.display='none';
+    containerRecupera.style.display='none'
+}
+btnIngresar.addEventListener('click',iniciarSesionFunction);
+nuevoCliente2.addEventListener('click',  nuevoRegistro);
+
+
+
+
