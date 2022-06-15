be7b8caf1393bab466034e2fd946b3db061ee7fb
camiimazzaro <camilamazzaro90@gmail.com>
2022-06-15 19:21:08 -0300

    creamos la sección - cotización del dólar -

--- home.js
+++ home.js
@@ -18,3 +18,45 @@ select.addEventListener('click', () => {
 	opciones.classList.toggle('active');
 
 });
+
+const list = document.getElementById('list-group');
+fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
+    .then((data) => data.json())
+    .then((data) =>
+        data.forEach((e) => {
+            console.log(e);
+            const li = document.createElement('li');
+            li.classList.add('list-group-item');
+            list.appendChild(li);
+
+			const nombreDolar = document.createElement('h4');
+			nombreDolar.innerText = `${e.casa.nombre}`;
+			li.appendChild(nombreDolar);
+			
+			const divCompraVenta = document.createElement('div');
+			divCompraVenta.classList.add('div-compra-venta');
+			li.appendChild(divCompraVenta);
+
+            const compra = document.createElement('h6');
+            compra.innerText = `Compra`;
+            divCompraVenta.appendChild(compra);
+
+            const venta = document.createElement('h6');
+            venta.innerText = `Venta`;
+            divCompraVenta.appendChild(venta);
+
+			const divPreciosDolar = document.createElement('div');
+			divPreciosDolar.classList.add('div-precios-dolar');
+			li.appendChild(divPreciosDolar);
+
+			const precioCompra = document.createElement('p');
+			precioCompra.innerText = `$ ${e.casa.compra}`;
+			divPreciosDolar.appendChild(precioCompra);
+
+			const precioVenta = document.createElement('p');
+			precioVenta.innerText = `$ ${e.casa.venta}`;
+			divPreciosDolar.appendChild(precioVenta);
+
+			
+        })
+    );
