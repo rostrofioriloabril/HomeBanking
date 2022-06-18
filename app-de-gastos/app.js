8d82d828e1569de2df1542a64aed47f6346845d1
Krawchik Ivan <krawchik.ivan@gmail.com>
2022-06-18 14:59:58 -0300

    cambio de estilo app de gastos

--- app-de-gastos/app.js
+++ app-de-gastos/app.js
@@ -83,6 +83,27 @@
 }())
 
 
+const select = document.querySelector('#select');
+const opciones = document.querySelector('#opciones');
+const salida = document.querySelector('.salida');
+const contenidoSelect = document.querySelector('#select .contenido-select');
+
+document.querySelectorAll('#opciones > .opcion').forEach((opcion) => {
+	opcion.addEventListener('click', (e) => {
+		e.preventDefault();
+		contenidoSelect.innerHTML = e.currentTarget.innerHTML;
+		select.classList.toggle('active');
+		select.style.background="#fff"
+		opciones.classList.toggle('active');
+	});
+});
+
+select.addEventListener('click', () => {
+	select.classList.toggle('active');
+	opciones.classList.toggle('active');
+
+});
+
 
 
 
