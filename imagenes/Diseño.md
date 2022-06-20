e8b25d1fbd008b356a526cb7be12e5ce6646cb78
Krawchik Ivan <krawchik.ivan@gmail.com>
2022-06-19 23:16:24 -0300

    Revert "Agregando el js al home, validaciones funcionanod. Falta enlazar el contenido del registro con el inicio y la recuperacion de contraseña"
    This reverts commit ad4500d016d6a507b33d903ae91ae5139f4fff29.

--- imagenes/Diseño.md
+++
@@ -1,2 +0,0 @@
-# Diseño original realizado en figma.
-https://www.figma.com/file/K6fnWaPUhd6ZyHrU1js5KO/MaquetaHB
--- main2.js
+++ main2.js
@@ -37,28 +37,85 @@ meOlvide.addEventListener('click', datosOlvidados);
 
 
 iniciarSesion.addEventListener('click', iniciarSesionFunction);
-
+registro.addEventListener('click',nuevoRegistro);
 
 
 function iniciarSesionFunction (event){
     event.preventDefault();
     containerPrincipal.style.display='block';
     containerAsociate.style.display='none';
-    containerRecupera.style.display='none';
+    containerRecupera.style.display='none'
 }
-
+btnIngresar.addEventListener('click',iniciarSesionFunction);
 nuevoCliente2.addEventListener('click',  nuevoRegistro);
 
- //ESCONDER Y MOSTRAR BALANCE 
-//let hideInfoButton: document.getElementById("hideInfo");
-//let balanceInfo: document.getElementsByClassName("balance_item");
-//
-//function hideBalance (){
-//    balanceInfo.innerHTML= "XX.XXX;XX"
-//}
 
-//hideInfoButton.addEventListener("click", hideBalance);
+//------------ formulario | validación ------------
 
+let dniInput = document.getElementsByClassName('dni'),
+    userInput = document.getElementsByClassName('user'),
+    passwordInput = document.getElementsByClassName('password'),
+    passwordRepeatInput = document.getElementsByClassName('passwordRepeat'),
+    nameInput = document.getElementsByClassName('name'),
+    lastNameInput = document.getElementsByClassName('lastName'),
+    ingresar = document.getElementsByClassName('btn-ingresar'),
+    dni = dniInput.value,
+    user = userInput.value,
+    password = passwordInput.value,
+    passwordRepeat = passwordRepeatInput.value,
+    name = nameInput.value,
+    lastName = lastNameInput.value;
+    
+
+function validarIngreso(){
+    validarDni(dni);
+    validarUser(user);
+    validarPassword(password);
+}
+function validarRegistro(){
+    validarName(name);
+    validarDni(dni);
+    validarUser(user);
+    validarPassword(password);
+}
+function validarDni(dni){
+    if(dni.length<8 || dni.length>8){
+        dniInput.style.border=('1px solid red !important');
+    }
+}
+function validarUser(user){
+    if(user.length<8){
+        userInput.style.border=('1px solid red !important');
+    }
+}
+function validarPassword(password){
+    if(password.length<8){
+        passwordInput.style.border=('1px solid red !important');
+    }
+    if (passwordRepeat!=''){
+        if(password!=passwordRepeat){
+            passwordInput.style.border=('1px solid red !important');
+            passwordRepeatInput.style.border=('1px solid red !important');
+        }
+    }
+}
+function validarName(name){
+    const letras = new RegExp("^[a-zA-Z ]+$");
+    return letras.test(name);
+}
+
+ingresar.addEventListener('click', validarIngreso);
+ 
+/* ESCONDER Y MOSTRAR BALANCE 
+let hideInfoButton: document.getElementById("hideInfo");
+let balanceInfo: document.getElementsByClassName("balance_item");
+
+function hideBalance (){
+    balanceInfo.innerHTML= "XX.XXX;XX"
+}
+ 
+hideInfoButton.addEventListener("click", hideBalance);
+*///
 
 
 
