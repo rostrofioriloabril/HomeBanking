44b504b4c7c0672086bde9806efa19b803448275
Fede Czer <you@example.com>
2022-07-26 10:52:40 -0300

    Avance punto 1

--- sprint6/1era problematica.sql
+++ sprint6/1era problematica.sql
@@ -1,7 +1,39 @@
 /* PRIMERA PROBLEMATICA */
 /* 1 */
+CREATE TABLE Tipos_Cliente(
+	type_account_id INTEGER PRIMARY KEY,
+	type_customer_name TEXT NOT NULL
+)
+
+CREATE TABLE Tipo_Cuenta (
+	type_account_id INTEGER PRIMARY KEY,
+	type_account_name TEXT NOT NULL
+)
+
+CREATE TABLE Marca_Tarjeta (
+	brand_card_id INTEGER PRIMARY KEY,
+	brand_card_name TEXT NOT NULL
+)
 
 /* 2 */
+CREATE TABLE Tarjeta (
+	card_id INTEGER PRIMARY KEY ,
+	card_number INTEGER CHECK (length(card_number <= 20)),
+	card_cvv INTEGER NOT NULL,
+	card_expired_date INTEGER NOT NULL,
+	card_ valid_date INTEGER NOT NULL,
+	card_type TEXT NOT NULL,
+	customer_id INTEGER NOT NULL
+	FOREIGN KEY (customer_id) 
+	REFERENCES cliente(customer_id)
+	ON UPDATE SET NULL
+	ON DELETE SET NULL
+	brand_card_id INTEGER NOT NULL
+	FOREIGN KEY (brand_card_id)
+	REFERENCES Marca_Tarjeta(brand_card_id)
+	ON UPDATE SET NULL
+	ON DELETE SET NULL
+)
 
 /* 3 */
 
