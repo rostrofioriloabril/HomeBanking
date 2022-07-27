05038d2ee61dfd4423ea78b2722a955992d71d5c
camiimazzaro <camilamazzaro90@gmail.com>
2022-07-27 17:14:57 -0300

    avances en la 2da problemática

--- sprint6/2da  problematica.sql
+++ sprint6/2da  problematica.sql
@@ -1,5 +1,19 @@
 /* SEGUNDA PROBLEMATICA */
 /* 1 */ FALTA PONER LA EDAD
+
+/*Hice la función para que reste la fecha actual con la fecha de nacimiento del cliente para calcular su edad. Falta mostrar en un Select el nombre, apellido, etc + esta subconsoluta de la edad*/
+SELECT (strftime('%Y', 'now') - strftime('%Y', cliente.dob )) - (strftime('%m-%d', 'now') < strftime('%m-%d', cliente.dob )) AS Fecha_Nacimiento
+FROM cliente;
+
+/*una idea de por donde puede ir lo de arriba*/
+SELECT customer_id AS ID, branch_id AS Sucursal_ID,customer_name AS Nombre,customer_surname AS Apellido, customer_DNI as DNI, dob AS edad
+FROM cliente
+WHERE(SELECT (strftime('%Y', 'now') - strftime('%Y', cliente.dob )) - (strftime('%m-%d', 'now') < strftime('%m-%d', cliente.dob )) AS Fecha_Nacimiento
+		FROM cliente
+		WHERE Fecha_Nacimiento = cliente.dob
+		) = edad
+
+
 SELECT customer_id AS ID, branch_id AS Sucursal_ID,customer_name AS Nombre,customer_surname AS Apellido, customer_DNI as DNI, dob AS edad
 FROM cliente
 WHERE edad>40
