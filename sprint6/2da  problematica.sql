7f12065996fd98883ac7985c5307b461b9869382
Krawchik Ivan <krawchik.ivan@gmail.com>
2022-07-28 12:56:22 -0300

    devolver la edad en una consulta

--- sprint6/2da  problematica.sql
+++ sprint6/2da  problematica.sql
@@ -1,26 +1,12 @@
 /* SEGUNDA PROBLEMATICA */
-/* 1 */ FALTA PONER LA EDAD
-
-/*Hice la función para que reste la fecha actual con la fecha de nacimiento del cliente para calcular su edad. Falta mostrar en un Select el nombre, apellido, etc + esta subconsoluta de la edad*/
-SELECT (strftime('%Y', 'now') - strftime('%Y', cliente.dob )) - (strftime('%m-%d', 'now') < strftime('%m-%d', cliente.dob )) AS Fecha_Nacimiento
-FROM cliente;
-
-/*una idea de por donde puede ir lo de arriba*/
-SELECT customer_id AS ID, branch_id AS Sucursal_ID,customer_name AS Nombre,customer_surname AS Apellido, customer_DNI as DNI, dob AS edad
-FROM cliente
-WHERE(SELECT (strftime('%Y', 'now') - strftime('%Y', cliente.dob )) - (strftime('%m-%d', 'now') < strftime('%m-%d', cliente.dob )) AS Fecha_Nacimiento
-		FROM cliente
-		WHERE Fecha_Nacimiento = cliente.dob
-		) = edad
-
-
-SELECT customer_id AS ID, branch_id AS Sucursal_ID,customer_name AS Nombre,customer_surname AS Apellido, customer_DNI as DNI, dob AS edad
+/* 1 */ LISTO
+SELECT customer_id AS ID, branch_id AS Sucursal_ID,customer_name AS Nombre,customer_surname AS Apellido, customer_DNI as DNI,(strftime('%Y', 'now') - strftime('%Y', cliente.dob )) - (strftime('%m-%d', 'now') < strftime('%m-%d', cliente.dob )) AS Edad
 FROM cliente
 WHERE edad>40
 ORDER BY 5 DESC
 
-SELECT (SELECT customer_id AS ID, branch_id AS Sucursal_ID,customer_name AS Nombre,customer_surname AS Apellido, customer_DNI as DNI, dob AS edad
-		FROM cliente)
+SELECT customer_id AS ID, branch_id AS Sucursal_ID,customer_name AS Nombre,customer_surname AS Apellido, customer_DNI as DNI,(strftime('%Y', 'now') - strftime('%Y', cliente.dob )) - (strftime('%m-%d', 'now') < strftime('%m-%d', cliente.dob )) AS Edad
+FROM cliente
 WHERE nombre LIKE 'Anne' OR nombre LIKE 'Tyler'
 ORDER BY edad
 
