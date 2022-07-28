054dc2c2db420186475d55cc03167f6b92c32a96
camiimazzaro <camilamazzaro90@gmail.com>
2022-07-28 14:26:08 -0300

    terminamos la 2da Problematica

--- sprint6/2da  problematica.sql
+++ sprint6/2da  problematica.sql
@@ -10,16 +10,26 @@ FROM cliente
 WHERE nombre LIKE 'Anne' OR nombre LIKE 'Tyler'
 ORDER BY edad
 
-/* 2 */
+/* 2 */ LISTO
+INSERT INTO cliente(customer_name, customer_surname, customer_DNI, dob, branch_id)
+VALUES	
+	('Lois', 'Stout', 47730534, '1984-07-07', 80), 
+	('Hall', 'Mcconnell', 52055464, '1968-04-30', 45),
+	('Hilel', 'Mclean', 43625213, '1993-03-28', 77),
+	('Jin', 'Cooley', 21207908, '1959-08-24', 96),
+	('Gabriel', 'Harmon', 57063950, '1976-04-01', 27)
 
-/* 3 */
+/* 3 */ LISTO
+UPDATE cliente
+SET branch_id = 10
+WHERE customer_id >= 503
 
 /* 4 */ LISTO
 DELETE FROM cliente
 WHERE customer_name='Noel' AND customer_surname='David';
 
-/* 5 */
-SELECT * LISTO
+/* 5 */ LISTO
+SELECT * 
 FROM prestamo
 ORDER BY 4 DESC
 LIMIT 1
