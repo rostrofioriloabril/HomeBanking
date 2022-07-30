9ce2b87ace15b61307b3d2fb709bf9a5c0e5c15b
camiimazzaro <camilamazzaro90@gmail.com>
2022-07-30 15:40:06 -0300

    terminamos los puntos 3 y 4, problematica 4

--- sprint6/4ta Problematica.sql
+++ sprint6/4ta Problematica.sql
@@ -7,14 +7,38 @@ ON sucursal.branch_id=cliente.branch_id
 GROUP BY Nombre_sucursal
 ORDER BY 1 DESC
 
-/* 2 */ (CANTIDAD DE EMPLEADOS x SUCURSAL, FALTA COMPARAR CONTRA CANTIDAD DE CLEINTES)
-SELECT COUNT(employee_id) AS Cantidad_empleados,sucursal.branch_name as Nombre_sucursal
+/* 2 */ (FALTA DIVIDIR CANTIDAD DE CLIENTES POR EMPLEADO, EN CADA SUCURSAL)
+SELECT COUNT(employee_id) AS Cantidad_empleados,sucursal.branch_name as Nombre_sucursal, customer_id as Cantidad_clientes
 FROM empleado
 INNER JOIN sucursal
 ON sucursal.branch_id=empleado.branch_id
-GROUP BY Nombre_sucursal
-/* 3 */
-/* 4 */
+INNER JOIN cliente
+ON cliente.branch_id = empleado.branch_id
+GROUP BY Nombre_sucursal 
+
+/* 3 */ LISTO
+SELECT COUNT(card_type) AS Tarjetas_credito, Marca_tarjeta.brand_card_name AS Marca_de_tarjeta, sucursal.branch_name AS Nombre_sucursal
+FROM Tarjeta
+INNER JOIN Marca_tarjeta
+ON Marca_tarjeta.brand_card_id = Tarjeta.brand_card_id
+INNER JOIN cliente
+ON cliente.customer_id = Tarjeta.customer_id
+INNER JOIN sucursal
+ON sucursal.branch_id = cliente.branch_id
+WHERE card_type = 'Credito'
+GROUP BY sucursal.branch_id,
+		Marca_tarjeta.brand_card_name
+
+
+/* 4 */ LISTO
+SELECT AVG(loan_total) AS Promedio_creditos, sucursal.branch_name AS Nombre_sucursal
+FROM prestamo
+INNER JOIN cliente
+ON cliente.customer_id = prestamo.customer_id
+INNER JOIN sucursal
+ON sucursal.branch_id = cliente.branch_id
+GROUP BY sucursal.branch_name
+
 /* 5 */
 /* 6 */
 /* 7 */
