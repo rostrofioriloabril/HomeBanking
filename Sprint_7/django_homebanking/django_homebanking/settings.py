a96b95a82a675e6ce805d18b85a6566b6a2424a5
camiimazzaro <camilamazzaro90@gmail.com>
2022-08-10 19:34:11 -0300

    corrigiendo un error

--- Sprint_7/django_homebanking/django_homebanking/settings.py
+++ Sprint_7/django_homebanking/django_homebanking/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Clientes',
+    'Clientes.apps.CuentasConfig',
     'Cuentas.apps.CuentasConfig',
     'Login.apps.LoginConfig',
     'Prestamos.apps.PrestamosConfig',
