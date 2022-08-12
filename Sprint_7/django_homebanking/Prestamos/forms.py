779c47c5bd6d81f7b25494ff59693e481a421e6a
camiimazzaro <camilamazzaro90@gmail.com>
2022-08-12 15:36:22 -0300

    eliminamos carpeta sprint7

--- Sprint_7/django_homebanking/Prestamos/forms.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import field
-from tkinter import Widget
-from django import forms
-from .models import Prestamos
-
-class PrestamosForm(forms.ModelForm):
-    class Meta:
-        model=Prestamos
-        fields=['loan_type','loan_date','loan_total','customer_id']
--- sprint7/homebanking/Prestamos/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .forms import PrestamosForm
-
-
-# Create your views here.
-
-def prestamos(request):
-    forms=PrestamosForm()
-    return render(request, "Prestamos/prestamos.html", {'form': forms})
