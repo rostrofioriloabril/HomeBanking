9c20139da9c2d13c81e72c7a1be0cf5699684d17
Abril Micaela Rostro Fiorilo <rostrofioriloabril@gmail.com>
2022-08-12 20:12:58 -0300

    Eliminando directorio Sprint_7

--- Sprint_7/django_homebanking/Clientes/forms.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import field
-from tkinter import Widget
-from django import forms
-from .models import Cliente
-
-class ClienteForm(forms.ModelForm):
-    class Meta:
-        model=Cliente
-        fields=['customer_name','customer_surname','customer_dni']
