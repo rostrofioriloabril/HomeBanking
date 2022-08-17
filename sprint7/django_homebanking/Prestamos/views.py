7cf49139a17c9c6ec734e6f761d3f803fe3598a0
Abril Micaela Rostro Fiorilo <rostrofioriloabril@gmail.com>
2022-08-17 20:03:20 -0300

    volviendo a subir todo por falla

--- sprint7/django_homebanking/Prestamos/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from . forms import PrestamosForm
-
-def prestamos(request):
-    forms=PrestamosForm()
-    return render(request, "Prestamos/prestamos.html", {'form': forms})
-
